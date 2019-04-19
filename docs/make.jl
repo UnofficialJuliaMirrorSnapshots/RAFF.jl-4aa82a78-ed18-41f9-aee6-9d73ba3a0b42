755fd71277f2bce1bf53b71b1fa9a68d8b3ccfa1
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-19 03:19:49 +0000

    Snapshot of branch dev taken on 2019-04-18T23:19:44.778-04:00 from "https://github.com/UnofficialJuliaMirror/RAFF.jl-4aa82a78-ed18-41f9-aee6-9d73ba3a0b42"

---
+++ docs/make.jl
@@ -0,0 +1,16 @@
+using Documenter, RAFF
+
+makedocs(
+    assets = ["assets/favicon.ico"],
+    sitename = "RAFF- Robust Algebraic Fitting Function",
+    pages = ["Overview" => "index.md",
+	     "Tutorial"=> "tutorial.md",
+	     "API" => "api.md"],
+    #html_prettyurls = false
+    #format = Documenter.HTML(prettyurls = false),
+    modules = [RAFF]
+	)
+	
+deploydocs(
+	repo = "github.com/fsobral/RAFF.jl.git"
+	)
